fcad1db5b0a72eb86a75ae75325f4cbb721f90a6
Mick Sato <syou222@gmail.com>
2017-12-11 23:20:17 +0000

    add react ver

---
+++ app/javascript/components/Reservations.js
@@ -0,0 +1,13 @@
+import React from "react"
+import PropTypes from "prop-types"
+class Reservations extends React.Component {
+  render () {
+    return (
+    <div>
+      hello. I'm Reservations
+    </div>
+    );
+  }
+}
+
+export default Reservations
